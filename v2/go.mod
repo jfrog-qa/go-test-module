e861b624dc7bcbd7c7c9e412f3afa3a16c6fd36f
barakh <barakh@jfrog.com>
2023-12-24 16:01:28 +0200

    v2.0.0

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/jfrog-qa/go-test-module/v2
+
+go 1.21.0
---
+++ v2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func helloworld() string {
+	return "JFrog test - version v2.0.0"
+}
+
+func main() {
+	fmt.Println(helloworld())
+}
