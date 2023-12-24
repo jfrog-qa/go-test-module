c57a6fdea4b702f7d6532a0f3bd818f909026c6b
barakh <barakh@jfrog.com>
2023-12-24 16:03:48 +0200

    sub v1.0.1

---
+++ submodule/go.mod
@@ -0,0 +1,3 @@
+module github.com/jfrog-qa/go-test-module/submodule
+
+go 1.21.0
---
+++ submodule/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func helloworld() string {
+	return "JFrog test submodule - version v1.0.1"
+}
+
+func main() {
+	fmt.Println(helloworld())
+}
